eb7edc97c7c6868738f7e6124476d8a0487389be
lucullusTheOnly <christian_ebberg@yahoo.de>
2019-03-10 21:27:20 +0100

    Added notifications for control

--- DailyXKCD.js
+++ DailyXKCD.js
@@ -4,8 +4,9 @@ Module.register("DailyXKCD", {
     defaults: {
         dailyJsonUrl: "http://xkcd.com/info.0.json",
         updateInterval: 10000 * 60 * 60, // 10 hours
-        invertColors: false
-
+        invertColors: false,
+        max_height: 500,
+        max_width: 500
     },
 
     start: function() {
@@ -16,6 +17,8 @@ Module.register("DailyXKCD", {
         this.dailyComicTitle = "";
         this.dailyComicAlt = "";
 
+        this.current_comic_number = 0;
+
         this.getComic();
     },
 
@@ -24,21 +27,24 @@ Module.register("DailyXKCD", {
         return ["moment.js"];
     },
 
-    getComic: function() {
+    getComic: function(number, randomize) {
         Log.info("XKCD: Getting comic.");
 
         this.sendSocketNotification("GET_COMIC", {
-            config: this.config
+            config: this.config,
+            number: number,
+            randomize: randomize
         });
     },
 
     socketNotificationReceived: function(notification, payload) {
 
         if (notification === "COMIC") {
-            Log.info(payload.img);
-            this.dailyComic = payload.img;
-            this.dailyComicTitle = payload.safe_title;
-            this.dailyComicAlt = payload.alt;
+            Log.info(payload.body.img+ "  ("+payload.body.num.toString()+")");
+            this.dailyComic = payload.body.img;
+            this.dailyComicTitle = payload.body.safe_title;
+            this.dailyComicAlt = payload.body.alt;
+            this.current_comic_number = payload.body.num;
             this.scheduleUpdate();
         }
 
@@ -47,6 +53,7 @@ Module.register("DailyXKCD", {
     // Override dom generator.
     getDom: function() {
         var wrapper = document.createElement("div");
+        wrapper.style.border = "thin solid #fff";
 
         var title = document.createElement("div");
         title.className = "bright large light";
@@ -58,6 +65,17 @@ Module.register("DailyXKCD", {
         xkcd.src = this.dailyComic;
         if (this.config.invertColors) {
                  xkcd.setAttribute("style", "-webkit-filter: invert(100%); max-width: 100%; max-height: 100%; height: 280px; ")
+        }
+
+        Log.log("X: "+xkcd.naturalWidth + ", Y:"+xkcd.naturalHeight);
+        if(this.dailyComic != ""){
+          if(xkcd.naturalWidth > xkcd.naturalHeight){
+            xkcd.style.width = this.config.max_width+"px";
+            xkcd.style.height = "";
+          } else {
+            xkcd.style.height = this.config.max_height+"px";
+            xkcd.style.width = "";
+          }
         }
 
 
@@ -84,6 +102,24 @@ Module.register("DailyXKCD", {
         setInterval(function() {
             self.getComic();
         }, this.config.updateInterval);
+    },
+
+    notificationReceived: function(notification, payload, sender){
+      var self=this;
+      switch(notification){
+        case "NEXT_COMIC":
+          self.getComic(this.current_comic_number + 1, false);
+          break;
+        case "PREVIOUS_COMIC":
+          self.getComic(this.current_comic_number - 1, false);
+          break;
+        case "SHOW_COMIC":
+          self.getComic(payload.number, false);
+          break;
+        case "RANDOM_COMIC":
+          self.getComic(-1, true);
+          break;
+      }
     }
 
 });
--- node_helper.js
+++ node_helper.js
@@ -10,7 +10,7 @@ module.exports = NodeHelper.create({
 	
 	socketNotificationReceived: function(notification, payload) {
 		var self = this;
-		console.log("Notification: " + notification + " Payload: " + payload);
+		//console.log("Notification: " + notification + " Payload: " + payload);
 		
 		if(notification === "GET_COMIC"){
 			
@@ -18,28 +18,36 @@ module.exports = NodeHelper.create({
 			
 			request(comicJsonUri, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
-                    var comicDays = [ 1,3,5 ];
-                    var dayNum = new Date().getDay(); 
-                    console.log("dayNum: "  + dayNum + " " + comicDays.indexOf(dayNum) );
-					if ( parseFloat(comicDays.indexOf(dayNum)) !== parseFloat("-1") ) {
-                            console.log("New Comic");
-							self.sendSocketNotification("COMIC", JSON.parse(body));
-					} else {
-                        console.log("Random Comic");
-                        var comic = JSON.parse(body);                         
-                        var rndcomic = Math.floor((Math.random() * comic.num) + 1); 
-                        var rndUrl = "http://xkcd.com/" + rndcomic.toString() + "/info.0.json";
-                        console.log("Random url: " + rndUrl);
-                        request(rndUrl, function (error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                self.sendSocketNotification("COMIC", JSON.parse(body));
-                            }
-                        });
+          var comicDays = [ 1,3,5 ];
+          var dayNum = new Date().getDay(); 
+          console.log("dayNum: "  + dayNum + " " + comicDays.indexOf(dayNum) );
+					if ( (parseFloat(comicDays.indexOf(dayNum)) == parseFloat("-1")
+              || payload.randomize == true)
+              && (payload.number == undefined || payload.number == -1)) {
+            //console.log("Random Comic");
+            var comic = JSON.parse(body);                         
+            var rndcomic = Math.floor((Math.random() * comic.num) + 1); 
+            var rndUrl = "http://xkcd.com/" + rndcomic.toString() + "/info.0.json";
+            console.log("Random url: " + rndUrl);
+            request(rndUrl, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    self.sendSocketNotification("COMIC", {body: JSON.parse(body)});
+                }
+            });
+					} else if(payload.number != undefined && payload.number > 0){
+            var Url = "http://xkcd.com/" + payload.number.toString() + "/info.0.json";
+            console.log("Specific comic url: " + Url);
+            request(Url, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    self.sendSocketNotification("COMIC", {body: JSON.parse(body)});
+                }
+            });
+          } else {
+            console.log("New Comic");
+            self.sendSocketNotification("COMIC", {body: JSON.parse(body), daily: true});
 					}
 				}
 			});
-
 		}
-		
 	},
 });
